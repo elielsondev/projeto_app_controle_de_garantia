7982d1eeb874667dba6196efa02dd6174e6ce0ee
LUCAS <lucasts.2110@gmail.com>
2025-12-14 14:21:01 -0300

    Header atualizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,38 @@
+import { useState } from "react";
 import perfilIcon from "../assets/perfil.png";
-import { Squash as Hamburger } from 'hamburger-react'
+import { Squash as Hamburger } from "hamburger-react";
+import Sidebar from "./Sidebar";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className="flex justify-between items-center px-5 h-[60px] bg-purple-800 text-black shadow">
-      <Hamburger size={20} color="#ffffff" />
-      {/* <button>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6H21V8H3V6Z" />
-        </svg>
-      </button> */}
-      <button
-                className="p-2 rounded-lg hover:bg-gray-100 transition"
-                aria-label="perfil"
-              >
-                <img
-                  src={perfilIcon}
-                  alt="perfil"
-                  className="w-6 h-6 object-contain"
-                />
-              </button>
-    </header>
+    <>
+      <header className="flex justify-between items-center px-5 h-15 bg-purple-800 shadow">
+        <Hamburger
+          size={20}
+          color="#ffffff"
+          toggled={menuOpen}
+          toggle={setMenuOpen}
+        />
+
+        <button
+          className="p-2 rounded-lg hover:bg-white/10 transition"
+          aria-label="perfil"
+        >
+          <img
+            src={perfilIcon}
+            alt="perfil"
+            className="w-6 h-6 object-contain"
+          />
+        </button>
+      </header>
+
+      <Sidebar
+        isOpen={menuOpen}
+        onClose={() => setMenuOpen(false)}
+      />
+    </>
   );
 };
 
