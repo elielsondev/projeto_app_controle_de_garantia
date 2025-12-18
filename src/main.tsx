d23da0771c1faaa669727f97fed407b4465c0d0e
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-12-18 02:11:33 -0300

    feat - adjust in componente main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './App.css'
+import App from './App.js'
+import { BrowserRouter } from 'react-router-dom'
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  );
+}
