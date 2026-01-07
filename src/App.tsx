6073090c8997cc78bde35d5a25aac4a98a335013
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-07 20:46:00 -0300

    start NoteScreen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import NoteScreen from "./pages/NoteScreen";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={ <Login /> } />
         <Route path="/home" element={ <Home /> } />
         <Route path="/register" element={ <Register /> } />
+        <Route path="/note" element={ <NoteScreen /> } />
       </Routes>
     </div>
   );
--- src/pages/NoteScreen.tsx
+++ src/pages/NoteScreen.tsx
@@ -1,9 +1,26 @@
-// import React from 'react'
+import { CircleCheckBig, FileText } from "lucide-react"
 
 // Componente da tela da nota
 function NoteScreen() {
+
   return (
-    <div>NoteScreen</div>
+    <div className="">
+
+      {/* Header */}
+      <div className="flex items-center justify-center">
+        <div className="flex p-3 rounded-full bg-[#558941]/15 text-[#478E2C] m-10 shadow">
+          <CircleCheckBig color="#478E2C" />
+          <p className="ml-2 font-medium">Garantia Ativa</p>
+        </div>
+      </div>
+
+      {/* Image Note */}
+      <div className="grid grid-cols-2">
+        <div className="border ">
+        <FileText size={50} />
+        </div>
+      </div>
+    </div>
   )
 }
 
