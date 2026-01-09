a670214ab4ce85ffd0feda81704069ec431e0278
PRISCILLA MARINHO <priscillamarinho2005@gmail.com>
2026-01-09 15:21:15 -0300

    Icon Change

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -21,26 +21,35 @@ const valueColors: Record<ResumoItem["status"], string> = {
   expired: "text-red-600",
 };
 
+const iconColors: Record<ResumoItem["status"], string> = {
+  total: "#724EBF",
+  expiring: "#FFBE07",
+  active: "#478E2C",
+  expired: "#D41414",
+};
+
 const SummaryCard = ({ title, value, status, isActive, onClick }: SummaryCardProps) => {
+  const Icon = statusIcons[status];
+  const iconColor = iconColors[status];
+
   return (
     <div
       onClick={() => onClick(status)}
       className={`
-        relative cursor-pointer rounded-2xl sm:rounded-3xl p-2 m-1 sm:p-4 md:p-5
+        relative cursor-pointer rounded-2xl sm:rounded-3xl p-6 m-1 sm:p-5 md:p-6
         shadow-md border transition-all duration-300
         ${isActive ? "border-purple-600 scale-95" : "hover:scale-98"}
       `}
     >
-      <p className={`text-lg sm:text-xl md:text-2xl font-bold ${valueColors[status]}`}>
-        {value}
-      </p>
-      <p className="text-xs sm:text-sm">{title}</p>
+      <div className="flex flex-col text-left">
+        <p className="font-medium text-xs sm:text-sm">{title}</p>
+        <p className={`text-lg sm:text-xl md:text-2xl font-bold ${valueColors[status]}`}>
+          {value}
+        </p>
+      </div>
 
-      <span className="absolute right-3 top-1/2 -translate-y-1/2 opacity-75">
-        {(() => {
-          const Icon = statusIcons[status];
-          return <Icon className="w-7 h-7" />;
-        })()}
+      <span className="absolute right-3 top-1/2 -translate-y-1/2">
+        <Icon className="w-7 h-7" style={{ color: iconColor }} />
       </span>
     </div>
   );
